a066ff59ba25160e82d85f33201f96cbd85b71bc
igor-tsap <tsap.ihor@gmail.com>
2026-03-01 10:50:40 +0200

    edit requirements.txt

--- backend/bend/main.py
+++ backend/bend/main.py
@@ -1,35 +1,3 @@
-# # from fastapi import FastAPI, HTTPException
-# # from fastapi.middleware.cors import CORSMiddleware
-# # from typing import Optional
-# # import mysql.connector
-# # from mysql.connector import Error
-# # from pydantic import BaseModel
-# # from models import Product
-# # from db import get_connection
-
-# # app = FastAPI()
-
-# # origins = [
-# #     "http://localhost:3000",
-# #     "http://localhost",
-# #     "http://localhost:5000",
-# # ]
-
-# # products = Product.all()
-# # print("All products in DB:")
-# # for p in products:
-# #     print(p)
-# from models import Product
-
-
-# products = Product.all()
-# for prod in products:
-#     print(prod)
-
-# # @app.get("/api/products")
-# # def get_products():
-# #     return Product.all()
-
 from fastapi import FastAPI, HTTPException
 from .models import Product
 from .db import get_connection
